5f202c61285f142a84b508e5e3e8b36a26b491ab
Andreas Milch <mail@andreasmilch.cc>
2023-09-28 18:00:15 +0200

    changes for switch to mysql

--- database/migrations/2023_09_14_104129_create_products_table.php
+++ database/migrations/2023_09_14_104129_create_products_table.php
@@ -13,9 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->integer('category_id')->unsigned();
-            $table->foreign('category_id')->references('id')
-                ->on('categories')->onDelete('cascade');
+            $table->foreignId('category_id');
             $table->string('name');
             $table->string('sku')->unique();
             $table->string('description')->nullable();
